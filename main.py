dee40c69f17f3b78e247bfb0515e5619d3508ce9
Peysone <peysone@MacBook-Pro-Peysone.local>
2023-06-11 17:23:02 +0200

    pierwszy commit

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Wykorzystując funkcję range(...), stwórz listę składającą się z 10 kolejnych liczb całkowitych, zaczynając od 5.
+"""
+
+# list_of_numbers = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+for i in range(5,15):
+    print(i)
+
+
+
+
